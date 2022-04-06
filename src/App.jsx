28e2b298d3305612aa1e2b1d811ffa0c0ba37f33
charlie-robin <charlie.robin.richardson@gmail.com>
2022-04-06 08:49:26 +0100

    (feat): Added RadioButtons component and updated the readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,39 @@
 import { useEffect, useState } from "react";
 import "./App.scss";
 import CardContainer from "./components/CardContainer/CardContainer";
+import RadioButtons from "./components/RadioButtons/RadioButtons";
 import RangeInput from "./components/RangeInput/RangeInput";
 
 const App = () => {
   const [users, setUsers] = useState([]);
   const [numberOfUsers, setNumberOfUsers] = useState(5);
+  const [filterBy, setFilterBy] = useState("all");
 
   const url = "https://randomuser.me/api";
 
-  const getUsers = async (resultNumber) => {
-    const res = await fetch(url + `?results=${resultNumber}`);
+  const getUsers = async (resultNumber, genderFilter) => {
+    let urlWithParams = url + `?results=${resultNumber}`;
+
+    if (genderFilter !== "all") {
+      urlWithParams += `&gender=${genderFilter}`;
+    }
+
+    const res = await fetch(urlWithParams);
     const data = await res.json();
+
     setUsers(data.results);
   };
 
   useEffect(() => {
-    getUsers(numberOfUsers);
-  }, [numberOfUsers]);
+    getUsers(numberOfUsers, filterBy);
+  }, [numberOfUsers, filterBy]);
 
   const handleInputChange = (event) => {
     setNumberOfUsers(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    setFilterBy(event.target.value);
   };
 
   return (
@@ -34,6 +47,7 @@ const App = () => {
         value={numberOfUsers}
         onChange={handleInputChange}
       />
+      <RadioButtons onClick={handleClick} options={["All", "Female", "Male"]} caption="Select User Gender:"/>
       <CardContainer cards={users} />
     </div>
   );
